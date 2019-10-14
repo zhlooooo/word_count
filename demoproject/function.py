f48bcdca609c360a5c98852bdc74827e2016886b
zhl <zhlooooo@126.com>
2019-10-14 12:46:06 +0800

    添加了app01

---
+++ demoproject/function.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    user_text = request.GET['text']
+    total_count = len(user_text)
+    return render(request, 'count.html',
+                  {'count': total_count, 'text': user_text})
+
+
+def about(request):
+    return render(request, 'about.html')
